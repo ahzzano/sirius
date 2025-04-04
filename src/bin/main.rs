f90bd63b6ba85fe61a2196907fbeea08ee93df8c
W. Santos <w.santos.gg@gmail.com>
2025-04-04 12:45:13 +0800

    feat: remove all apps from main

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -66,75 +66,75 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
-    let mut wifi = WiFi::new(&_init, peripherals.WIFI);
-    wifi.init();
-    let mut sniffer = WifiSniffer::new(RefCell::new(wifi));
+    // let mut wifi = WiFi::new(&_init, peripherals.WIFI);
+    // wifi.init();
+    // let mut sniffer = WifiSniffer::new(RefCell::new(wifi));
 
-    sniffer.set_callback(|_packet| {
-        // println!("{data:2x?}")
-        let _ = match_frames!(_packet.data, beacon=BeaconFrame => {
-            let ssid = beacon.ssid();
-            print!("{ssid:?}");
-            let recv = beacon.header.receiver_address;
-            let transmitter = beacon.header.transmitter_address;
-            print!(" {transmitter:?} {recv:?}\n")
-        });
-    });
-    sniffer.init();
-    sniffer.enable();
-    sniffer.disable();
+    // sniffer.set_callback(|_packet| {
+    //     // println!("{data:2x?}")
+    //     let _ = match_frames!(_packet.data, beacon=BeaconFrame => {
+    //         let ssid = beacon.ssid();
+    //         print!("{ssid:?}");
+    //         let recv = beacon.header.receiver_address;
+    //         let transmitter = beacon.header.transmitter_address;
+    //         print!(" {transmitter:?} {recv:?}\n")
+    //     });
+    // });
+    // sniffer.init();
+    // sniffer.enable();
+    // sniffer.disable();
 
-    let spi = Spi::new(
-        peripherals.SPI2,
-        Config::default()
-            .with_frequency(Rate::from_khz(100))
-            .with_mode(esp_hal::spi::Mode::_0),
-    )
-    .unwrap()
-    .with_miso(peripherals.GPIO19)
-    .with_mosi(peripherals.GPIO5)
-    .with_sck(peripherals.GPIO18);
+    // let spi = Spi::new(
+    //     peripherals.SPI2,
+    //     Config::default()
+    //         .with_frequency(Rate::from_khz(100))
+    //         .with_mode(esp_hal::spi::Mode::_0),
+    // )
+    // .unwrap()
+    // .with_miso(peripherals.GPIO19)
+    // .with_mosi(peripherals.GPIO5)
+    // .with_sck(peripherals.GPIO18);
 
-    let mut _delays = Delay::new();
+    // let mut _delays = Delay::new();
 
-    let cs = Output::new(peripherals.GPIO17, Level::High, OutputConfig::default());
+    // let cs = Output::new(peripherals.GPIO17, Level::High, OutputConfig::default());
 
-    let atomic_cell = AtomicCell::new(spi);
-    let nrf_dev = AtomicDevice::new(&atomic_cell, cs, _delays).unwrap();
-    let mut delays = Delay::new();
+    // let atomic_cell = AtomicCell::new(spi);
+    // let nrf_dev = AtomicDevice::new(&atomic_cell, cs, _delays).unwrap();
+    // let mut delays = Delay::new();
 
-    let ce = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
+    // let ce = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
 
-    let mut nrf = Nrf24l01::new(
-        nrf_dev,
-        ce,
-        &mut delays,
-        NrfConfig::default()
-            .channel(11)
-            .pa_level(nrf24_rs::config::PALevel::Min)
-            .payload_size(MAX_PAYLOAD_SIZE)
-            .addr_width(5)
-            .crc_encoding_scheme(nrf24_rs::config::EncodingScheme::NoRedundancyCheck),
-    )
-    .unwrap();
+    // let mut nrf = Nrf24l01::new(
+    //     nrf_dev,
+    //     ce,
+    //     &mut delays,
+    //     NrfConfig::default()
+    //         .channel(11)
+    //         .pa_level(nrf24_rs::config::PALevel::Min)
+    //         .payload_size(MAX_PAYLOAD_SIZE)
+    //         .addr_width(5)
+    //         .crc_encoding_scheme(nrf24_rs::config::EncodingScheme::NoRedundancyCheck),
+    // )
+    // .unwrap();
 
-    let connectivity = nrf.is_connected();
+    // let connectivity = nrf.is_connected();
 
-    if let Ok(connected) = connectivity {
-        info!("NRF Status > {connected}");
-    }
+    // if let Ok(connected) = connectivity {
+    //     info!("NRF Status > {connected}");
+    // }
 
-    let _ = nrf.open_reading_pipe(DataPipe::DP0, b"0000");
-    let _ = nrf.start_listening();
+    // let _ = nrf.open_reading_pipe(DataPipe::DP0, b"0000");
+    // let _ = nrf.start_listening();
 
-    let mut buf = [0u8; MAX_PAYLOAD_SIZE as usize];
-    while nrf.data_available().is_ok() {
-        let res = nrf.read(&mut buf);
+    // let mut buf = [0u8; MAX_PAYLOAD_SIZE as usize];
+    // while nrf.data_available().is_ok() {
+    //     let res = nrf.read(&mut buf);
 
-        if res.is_ok() {
-            println!("Read: {buf:02x?}");
-        }
-    }
+    //     if res.is_ok() {
+    //         println!("Read: {buf:02x?}");
+    //     }
+    // }
 
     // TODO: Spawn some tasks
     let _ = spawner;
