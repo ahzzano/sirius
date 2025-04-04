dd4ebcb986e68a0e7676170137ffc24c33a8d52d
W. Santos <w.santos.gg@gmail.com>
2025-04-04 17:48:53 +0800

    feat: add SyncApp and AsyncApp

--- src/apps.rs
+++ src/apps.rs
@@ -1,9 +1,15 @@
 pub mod nrf_sniffer;
-pub mod sniffer;
+pub mod wifi_sniffer;
 
-pub trait App {
+pub struct Sync;
+pub struct Async;
+
+pub trait SyncApp {
     fn enable(&mut self);
-    // fn run(&mut self);
     fn disable(&mut self);
     fn is_enabled(&mut self) -> bool;
+}
+
+pub trait AsyncApp {
+    fn task(&mut self); // the thing that gets sent to Embassy
 }
