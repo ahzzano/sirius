50447d402eff13568a3222074af902fca45d1635
W. Santos <w.santos.gg@gmail.com>
2025-04-04 17:49:24 +0800

    feat: change sniffer to new wifi sniffer

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,11 +1,13 @@
 #![no_std]
 #![no_main]
 
+use core::cell::RefCell;
+
 use log::error;
 #[allow(unused_imports)]
-use sirius::apps::sniffer::WifiSniffer;
-#[allow(unused_imports)]
-use sirius::apps::App;
+use sirius::apps::wifi_sniffer::WifiSniffer;
+use sirius::apps::AsyncApp;
+use sirius::apps::SyncApp;
 #[allow(unused_imports)]
 use sirius::devices::wifi::WiFi;
 
@@ -49,82 +51,17 @@ async fn main(spawner: Spawner) {
     )
     .unwrap();
 
-    // let mut wifi = WiFi::new(&_init, peripherals.WIFI);
-    // wifi.init();
-    // let mut sniffer = WifiSniffer::new(RefCell::new(wifi));
+    let wifi = WiFi::new(&_init, peripherals.WIFI);
+    let mut sniff_sync = WifiSniffer::new(RefCell::new(wifi));
 
-    // sniffer.set_callback(|_packet| {
-    //     // println!("{data:2x?}")
-    //     let _ = match_frames!(_packet.data, beacon=BeaconFrame => {
-    //         let ssid = beacon.ssid();
-    //         print!("{ssid:?}");
-    //         let recv = beacon.header.receiver_address;
-    //         let transmitter = beacon.header.transmitter_address;
-    //         print!(" {transmitter:?} {recv:?}\n")
-    //     });
-    // });
-    // sniffer.init();
-    // sniffer.enable();
-    // sniffer.disable();
-
-    // let spi = Spi::new(
-    //     peripherals.SPI2,
-    //     Config::default()
-    //         .with_frequency(Rate::from_khz(100))
-    //         .with_mode(esp_hal::spi::Mode::_0),
-    // )
-    // .unwrap()
-    // .with_miso(peripherals.GPIO19)
-    // .with_mosi(peripherals.GPIO5)
-    // .with_sck(peripherals.GPIO18);
-
-    // let mut _delays = Delay::new();
-
-    // let cs = Output::new(peripherals.GPIO17, Level::High, OutputConfig::default());
-
-    // let atomic_cell = AtomicCell::new(spi);
-    // let nrf_dev = AtomicDevice::new(&atomic_cell, cs, _delays).unwrap();
-    // let mut delays = Delay::new();
-
-    // let ce = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
-
-    // let mut nrf = Nrf24l01::new(
-    //     nrf_dev,
-    //     ce,
-    //     &mut delays,
-    //     NrfConfig::default()
-    //         .channel(11)
-    //         .pa_level(nrf24_rs::config::PALevel::Min)
-    //         .payload_size(MAX_PAYLOAD_SIZE)
-    //         .addr_width(5)
-    //         .crc_encoding_scheme(nrf24_rs::config::EncodingScheme::NoRedundancyCheck),
-    // )
-    // .unwrap();
-
-    // let connectivity = nrf.is_connected();
-
-    // if let Ok(connected) = connectivity {
-    //     info!("NRF Status > {connected}");
-    // }
-
-    // let _ = nrf.open_reading_pipe(DataPipe::DP0, b"0000");
-    // let _ = nrf.start_listening();
-
-    // let mut buf = [0u8; MAX_PAYLOAD_SIZE as usize];
-    // while nrf.data_available().is_ok() {
-    //     let res = nrf.read(&mut buf);
-
-    //     if res.is_ok() {
-    //         println!("Read: {buf:02x?}");
-    //     }
-    // }
+    sniff_sync.init();
+    sniff_sync.enable();
 
     // TODO: Spawn some tasks
     let _ = spawner;
 
     loop {
-        // info!("Hello world!");
-        // println!("NRF Status: {nrf_dev:?}");
+        info!("Hello world!");
         Timer::after(Duration::from_secs(1)).await;
     }
 
